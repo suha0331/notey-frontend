71936874469f02a533f9771b2e45bddd3bd859b5
Lindsey McDowell <Lindsey@Lindseys-MacBook-Pro.local>
2018-02-01 21:19:22 -0800

    lol

---
+++ client/src/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDom from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { browserHistory, Router } from 'react-router';
+import routes from './routes.js';
+
+class App extends React.Component {
+    render() {
+        return (
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={browserHistory} routes={routes} />
+  </MuiThemeProvider>
+        )
+    }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -2,28 +2,33 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const app = express();
+var stormpath = require('express-stormpath');
+
 const PORT = process.env.PORT || 3000;
+const app = express();
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 // Serve up static assets
 app.use(express.static("client/build"));
+
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
+
+
+// Database
 var databaseUri = 'mongodb://localhost/noteyDb';
-
 if(process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 } else {
   mongoose.connect(databaseUri);
 }
-
 var db = mongoose.connection;
-
+// --------------------------------------------------------
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+// app.listen(PORT, function() {
+//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// });
