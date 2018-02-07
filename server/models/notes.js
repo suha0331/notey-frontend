59c416f364eb2f5ca237a29149bd107a9d33bf01
Lindsey McDowell <Lindsey@Lindseys-MacBook-Pro.local>
2018-02-06 23:56:08 -0800

    working with json for the notes to appear

--- server/models/notes.js
+++ server/models/notes.js
@@ -1,20 +1,29 @@
-// Require Mongoose + Moment
+// Require Mongoose
 var mongoose = require('mongoose');
+
 // Create a Schema Class
 var Schema = mongoose.Schema;
-// Moment Stuff
-var moment = require('moment');
-var now = moment();
 
-// Create Comment Schema
+// Create Note Schema
 var NoteSchema = new Schema({
-	notes: { header: String}, 
-	body: String, 
-	date: Date 
-})
 
-// Create the User model with Mongoose
-var Note = mongoose.model('Note', NoteSchema);
+  // Note Content
+  header: {
+    type: String
+  },
+    // Note Content
+  body: {
+    type: String
+  },
+  date: {
+  	type: String
+  }
+  
+});
+
+
+// Create the Note model with Mongoose
+var Note = mongoose.model( 'Note', NoteSchema);
 
 // Export the Model
 module.exports = Note;
