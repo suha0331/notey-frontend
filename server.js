9e17c67f75aedae70a26bcd69245a7b971202c61
Lindsey McDowell <Lindsey@Lindseys-MacBook-Pro.local>
2018-02-08 23:01:06 -0800

    hehe

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const jwt = require('jsonwebtoken');
 const passport = require("passport");
 const config = require('./config/index.json');
-require('./server/models').connect(config.dbUri);
 const authRoute = require("./server/routes/auth.js");
 // const apiRoute = require("./server/routes/api.js");
 var cors = require('cors')
@@ -37,15 +36,16 @@ app.use("/auth", authRoute);
 
 app.use(cors())
 
-// Database
+Database
 // var databaseUri = 'mongodb://localhost/noteyDb';
-// if(process.env.MONGODB_URI) {
-//   mongoose.connect(process.env.MONGODB_URI);
-// } else {
-//   mongoose.connect(databaseUri);
-// }
-// var db = mongoose.connection;
-// --------------------------------------------------------
+
+if(process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+require('./server/models').connect(config.dbUri);
+}
+var db = mongoose.connection;
+--------------------------------------------------------
 
 // const express = require('express');
 var router = express.Router();
@@ -126,30 +126,6 @@ app.delete("/notes/:userID/:id", function(req, res) {
 
         })})
 
-    // User.findOne({'_id': req.params.id}, {"notes": [] }, function(error, found) {
-    //     // Throw any errors to the console
-    //     if (error) {
-    //         console.log(error);
-    //     }
-    //     // If there are no errors, send the data to the browser as json
-    //     else {
-    //         console.log(found)
-    //         res.json(found);
-    //     }
-    // });
-
-
-// // Get Specific Note from specific user
-// app.delete("/notes/:id", function(req, res) {
-// // var userID = req.params.userID
-
-// Note.findByIdAndRemove(req.params.id), function(err) {
-//         if (error) {
-//             res.send(error);
-//         } else { res.send(post) }
-//     }
-
-// })
 
 // Start the API server
 app.listen(PORT, function() {
